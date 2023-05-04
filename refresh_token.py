310c2c81735eab673a58e8dca9edd96f686d84b0
Víctor Hernández <vic.her.ag@gmail.com>
2023-05-04 12:40:26 +0200

    Added reference to script creator

--- refresh_token.py
+++ refresh_token.py
@@ -1,6 +1,8 @@
-# Taken from https://www.jcchouinard.com/get-reddit-api-credentials-with-praw/
-
 #!/usr/bin/env python
+
+####
+# Taken from https://www.jcchouinard.com/get-reddit-api-credentials-with-praw/
+####
 
 """This example demonstrates the flow for retrieving a refresh token.
 
